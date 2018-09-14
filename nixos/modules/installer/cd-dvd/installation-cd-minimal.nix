72e4f16981fddfc590d4dbac19f65dddfbae1bec
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-09-14 21:17:00 +0200

    bcachefs iso

--- nixos/modules/installer/cd-dvd/installation-cd-minimal.nix
+++ nixos/modules/installer/cd-dvd/installation-cd-minimal.nix
@@ -6,5 +6,10 @@
 {
   imports =
     [ ./installation-cd-base.nix
-    ];
+     # Provide an initial copy of the NixOS channel so that the user
+     # doesn't need to run "nix-channel --update" first.
+     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+   ];
+
+  boot.supportedFilesystems = [ "zfs" "bcachefs" ];
 }
